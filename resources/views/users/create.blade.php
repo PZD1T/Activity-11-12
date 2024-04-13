29ef2de035d9c41214e910d800ba7f1f17a69910
PZD1T <161904089+PZD1T@users.noreply.github.com>
2024-04-12 22:46:26 -0600

    Commit Inicial

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div class="container">
+    
+<x-header-component titulo="Create Usuarios"></x-header-component>
+
+
+    <div class="row justify-content-center">
+       <div class="col-12 col-md-9 col-lg-6">
+                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis, similique inventore? Autem omnis iste ab facilis necessitatibus neque laborum provident vitae deserunt atque! </p>
+                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis, similique inventore? Autem omnis iste ab facilis necessitatibus neque laborum provident vitae deserunt atque! </p>
+
+       </div>
+    </div>
+</div>
+@endsection
+
+
